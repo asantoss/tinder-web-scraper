0358e0d341784132b18e2c05ebc58b1d8a866d5d
Jaafar Skafi <jaafarskafi1@gmail.com>
2018-01-20 00:07:08 -0800

    bug fixes

--- index.js
+++ index.js
@@ -29,25 +29,25 @@ async function moveToNextCard () {
 
 async function iterateThroughPhotos(elements) {
   for (let i = 1; i < elements.length; i++) {
-    const locatePhotoElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div' + '/div[' + i + ']/div/img')), 120000);
-    const getSourceDownloadImageFile = await locatePhotoElement.getAttribute('src');
+    let locatePhotoElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div' + '/div[' + i + ']/div/img')), 120000);
+    let getSourceDownloadImageFile = await locatePhotoElement.getAttribute('src');
     let file = fs.createWriteStream(__dirname + '/images/' + getSourceDownloadImageFile.split('').join('').slice(getSourceDownloadImageFile.length-10, getSourceDownloadImageFile.length));
     let request = https.get(getSourceDownloadImageFile, (response) => {
       response.pipe(file);
     });
-    const moveToNextPhoto = await driver.actions().sendKeys(webdriver.Key.SPACE).perform();
+    let moveToNextPhoto = await driver.actions().sendKeys(webdriver.Key.SPACE).perform();
   }
 }
 
-async function swipeCards (numberOfCards) {
+async function swipeCardsFunction (numberOfCards) {
   for (let i = 0; i < numberOfCards; i++) {
-    const waitTillCardShows = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 120000);
-    const openProfile = await driver.actions().sendKeys(webdriver.Key.ARROW_UP).perform();
-    const waitTillCardOpens = driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div')), 120000);
-    const locatePhotos = By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div/div');
-    const findElements = driver.findElements();
-    const iterate = iterateThroughPhotos(findElements);
-    const moveToNextProfile = await moveToNextCard();
+    let waitTillCardShows = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 120000);
+    let openProfile = await driver.actions().sendKeys(webdriver.Key.ARROW_UP).perform();
+    let waitTillCardOpens = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div')), 120000);
+    let locatePhotos = await driver.findElements(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div/div'));
+    // let findElements = await driver.findElements();
+    let iterate = await iterateThroughPhotos(locatePhotos);
+    let moveToNextProfile = await moveToNextCard();
   }
 }
 
@@ -59,9 +59,9 @@ async function scrape () {
   const goToTinder = await driver.get('http://www.tinder.com/');
   const clickTinderLoginButton = await driver.wait(until.elementLocated(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')), 120000);
   const clickOpenCardButton = driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')).click();
-  const swipeCards = await swipeCards(300);
+  const swipeCards = await swipeCardsFunction(300);
 }
 
 scrape();
 
-driver.quit();
+// driver.quit();
