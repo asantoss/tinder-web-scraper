196f8cf461639a923829e779e268ee2b192be766
Jaafar Skafi <jaafarskafi1@gmail.com>
2018-04-30 17:19:32 -0700

    update dependencies

--- index.js
+++ index.js
@@ -22,31 +22,32 @@ let driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build(
 
 async function moveToNextCard () {
   let closeProfile = await driver.actions().sendKeys(webdriver.Key.ARROW_DOWN).perform();
-  let swipeLeft = await driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[2]')).click();
+  // let swipeLeft = await driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[2]')).click();
+  let swipeRight = await driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[4]')).click();
   let ensureCardExit = await driver.actions().sendKeys(webdriver.Key.ESCAPE).perform();
 }
 
 
-async function iterateThroughPhotos(elements) {
-  for (let i = 1; i < elements.length; i++) {
-    let locatePhotoElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div' + '/div[' + i + ']/div/img')), 120000);
-    let getSourceDownloadImageFile = await locatePhotoElement.getAttribute('src');
-    let file = fs.createWriteStream(__dirname + '/images/' + getSourceDownloadImageFile.split('').join('').slice(getSourceDownloadImageFile.length-10, getSourceDownloadImageFile.length));
-    let request = https.get(getSourceDownloadImageFile, (response) => {
-      response.pipe(file);
-    });
-    let moveToNextPhoto = await driver.actions().sendKeys(webdriver.Key.SPACE).perform();
-  }
-}
+// async function iterateThroughPhotos(elements) {
+//   for (let i = 1; i < elements.length; i++) {
+//     let locatePhotoElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div' + '/div[' + i + ']/div/img')), 120000);
+//     let getSourceDownloadImageFile = await locatePhotoElement.getAttribute('src');
+//     let file = fs.createWriteStream(__dirname + '/images/' + getSourceDownloadImageFile.split('').join('').slice(getSourceDownloadImageFile.length-10, getSourceDownloadImageFile.length));
+//     let request = https.get(getSourceDownloadImageFile, (response) => {
+//       response.pipe(file);
+//     });
+//     let moveToNextPhoto = await driver.actions().sendKeys(webdriver.Key.SPACE).perform();
+//   }
+// }
 
 async function swipeCardsFunction (numberOfCards) {
   for (let i = 0; i < numberOfCards; i++) {
     let waitTillCardShows = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 120000);
-    let openProfile = await driver.actions().sendKeys(webdriver.Key.ARROW_UP).perform();
-    let waitTillCardOpens = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div')), 120000);
-    let locatePhotos = await driver.findElements(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div/div'));
+    // let openProfile = await driver.actions().sendKeys(webdriver.Key.ARROW_UP).perform();
+    // let waitTillCardOpens = await driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div')), 120000);
+    // let locatePhotos = await driver.findElements(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div/div'));
     // let findElements = await driver.findElements();
-    let iterate = await iterateThroughPhotos(locatePhotos);
+    // let iterate = await iterateThroughPhotos(locatePhotos);
     let moveToNextProfile = await moveToNextCard();
   }
 }
@@ -55,11 +56,11 @@ async function scrape () {
   let goToFacebook = await driver.get('http://www.facebook.com/');
   let typeFacebookEmail = await driver.findElement(By.name('email')).sendKeys(secret.email);
   let typeFacebookPassword = await driver.findElement(By.name('pass')).sendKeys(secret.password);
-  let clickFacebookLoginButton = await driver.findElement(By.id('loginbutton')).click();
+  let clickLoginButton = await driver.findElement(By.id('loginbutton')).click();
   let goToTinder = await driver.get('http://www.tinder.com/');
-  let clickTinderLoginButton = await driver.wait(until.elementLocated(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')), 120000);
-  let clickOpenCardButton = driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')).click();
-  let swipeCards = await swipeCardsFunction(300);
+  let waitForLoginPopup = await driver.wait(until.elementLocated(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div/div[3]/button[1]')), 120000);
+  let clickLoginViaFacebook = driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div/div[3]/button[1]')).click();
+  let swipeCards = await swipeCardsFunction(5000);
 }
 
 scrape();
